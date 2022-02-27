7948387f0309b66728e4e4090211611bdc001b38
Shubham Pawar <shubhampawar3007@gmail.com>
2022-02-27 15:19:30 +0530

    default is my config, doom is for doom-emacs config

--- .emacs-profiles.el
+++ .emacs-profiles.el
@@ -1,4 +1,4 @@
-(("mine" . ((user-emacs-directory   . "~/.config/emacs-my")))
+(("default" . ((user-emacs-directory   . "~/.config/emacs-my")))
  ("alt"  . ((user-emacs-directory   . "~/.config/emacs-alt")))
- ("default" . ((user-emacs-directory   . "~/.config/emacs-doom")
+ ("doom" . ((user-emacs-directory   . "~/.config/emacs-doom")
                 (env . (("DOOMDIR"      . "~/.config/doom"))))))
