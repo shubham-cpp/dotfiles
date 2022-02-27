15a00ccd2e9f641e822c31696c78982a5d8fcb65
Shubham Pawar <shubhampawar3007@gmail.com>
2022-02-27 15:19:12 +0530

    If block for native-comp, changed fonts for daemon mode and disabled tab-bar

--- .config/emacs-my/early-init.el
+++ .config/emacs-my/early-init.el
@@ -4,23 +4,36 @@
 ;; The default is 800 kilobytes.  Measured in bytes.
 (setq gc-cons-threshold most-positive-fixnum
       gc-cons-percentage 0.6
+      load-prefer-newer noninteractive ;; Prefer loading newest compiled .el file
+      package-enable-at-startup nil ;; Don't use package.el, we'll use straight.el instead
       file-name-handler-alist nil
       site-run-file nil
       read-process-output-max (* 10 1024 1024)
-      load-prefer-newer noninteractive ;; Prefer loading newest compiled .el file
-      package-enable-at-startup nil ;; Don't use package.el, we'll use straight.el instead
       bidi-inhibit-bpa t)
+
+;; Native compilation settings
+(when (featurep 'native-compile)
+  ;; Silence compiler warnings as they can be pretty disruptive
+  (setq native-comp-async-report-warnings-errors nil)
+
+  ;; Make native compilation happens asynchronously
+  (setq native-comp-deferred-compilation t)
+
+  ;; Set the right directory to store the native compilation cache
+  (add-to-list 'native-comp-eln-load-path (expand-file-name "~/.cache/emacs-my/eln-cache/")))
 
 (defvar ian/gc-cons-threshold (* 100 1024 1024))
 
 ;; Remove some unneeded UI elements (the user can turn back on anything they wish)
 (setq inhibit-startup-message t)
+(push '(tab-bar-mode . 0) default-frame-alist)
 (push '(tool-bar-lines . 0) default-frame-alist)
 (push '(menu-bar-lines . 0) default-frame-alist)
 (push '(vertical-scroll-bars) default-frame-alist)
 (push '(background-color . "#232635") default-frame-alist)
 (push '(foreground-color . "#FFFFFF") default-frame-alist)
 (push '(mouse-color . "white") default-frame-alist)
+(push '(font . "JetBrainsMono Nerd Font-11") default-frame-alist)
 
 ;; Make the initial buffer load faster by setting its mode to fundamental-mode
 (setq initial-major-mode 'fundamental-mode)
