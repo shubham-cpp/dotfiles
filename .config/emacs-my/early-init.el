11c4938ca86252866ab5d7701c1789ebf3ec3dd9
Shubham Pawar <shubhampawar3007@gmail.com>
2022-03-17 12:03:49 +0530

    moved all setq inside eval-and-compile

--- .config/emacs-my/early-init.el
+++ .config/emacs-my/early-init.el
@@ -1,20 +1,16 @@
 ;;; early-init.el -*- lexical-binding: t; -*-
 
-;; Increase the GC threshold for faster startup
-;; The default is 800 kilobytes.  Measured in bytes.
-(setq gc-cons-threshold most-positive-fixnum
-      gc-cons-percentage 0.6
-      load-prefer-newer noninteractive ;; Prefer loading newest compiled .el file
-      package-enable-at-startup nil ;; Don't use package.el, we'll use straight.el instead
-      file-name-handler-alist nil
-      site-run-file nil
-      read-process-output-max (* 10 1024 1024)
-      bidi-inhibit-bpa t)
-
 ;; Native compilation settings
 (when (featurep 'native-compile)
   ;; Silence compiler warnings as they can be pretty disruptive
-  (setq native-comp-async-report-warnings-errors nil)
+  (eval-and-compile
+    (setq native-comp-async-report-warnings-errors nil
+	  native-comp-speed 2
+	  comp-speed 2
+	  comp-async-report-warnings-errors nil
+	  native-comp-async-query-on-exit t
+	  comp-async-query-on-exit t)
+    )
 
   ;; Make native compilation happens asynchronously
   (setq native-comp-deferred-compilation t)
@@ -22,9 +18,27 @@
   ;; Set the right directory to store the native compilation cache
   (add-to-list 'native-comp-eln-load-path (expand-file-name "~/.cache/emacs-my/eln-cache/")))
 
+(eval-and-compile
+  (setq gc-cons-threshold most-positive-fixnum
+	gc-cons-percentage 0.6
+	load-prefer-newer noninteractive
+	package-enable-at-startup nil
+	file-name-handler-alist nil
+	site-run-file nil
+	read-process-output-max (* 10 1024 1024)
+	bidi-inhibit-bpa t
+	highlight-nonselected-windows nil
+	frame-inhibit-implied-resize t
+	frame-resize-pixelwise t
+	fast-but-imprecise-scrolling t
+	redisplay-skip-fontification-on-input t))
+
+(setq-default bidi-display-reordering 'left-to-right
+	      bidi-paragraph-direction 'left-to-right
+	      cursor-in-non-selected-windows nil)
+
 (defvar ian/gc-cons-threshold (* 100 1024 1024))
 
-;; Remove some unneeded UI elements (the user can turn back on anything they wish)
 (setq inhibit-startup-message t)
 (push '(tab-bar-mode . 0) default-frame-alist)
 (push '(tool-bar-lines . 0) default-frame-alist)
@@ -41,8 +55,7 @@
 (add-hook 'emacs-startup-hook ; hook run after loading init files
           #'(lambda ()
               (setq gc-cons-threshold ian/gc-cons-threshold
-                    gc-cons-percentage 0.1
-                    file-name-handler-alist file-name-handler-alist-original)))
+                    gc-cons-percentage 0.1)))
 
 (add-hook 'minibuffer-setup-hook #'(lambda ()
                                      (setq gc-cons-threshold most-positive-fixnum)))
