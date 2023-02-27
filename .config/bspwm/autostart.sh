fed5ef2e6e127c1c5ec787632c27fdbb263288ac
Shubham Pawar <shubhampawar3007@gmail.com>
2023-02-27 11:21:26 +0530

    start main polybar for bspwm

--- .config/bspwm/autostart.sh
+++ .config/bspwm/autostart.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
 run() {
-    if ! pgrep $1 ; then
-        setsid -f $@
-    fi
+	if ! pgrep $1; then
+		setsid -f $@
+	fi
 }
-
-~/.config/polybar/launch.sh &
+pgrep polybar | xargs -r kill -9
+# Wait until the processes have been shut down
+while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
+polybar main -c "$HOME"/.config/polybar/config.ini &
+# ~/.config/polybar/launch.sh &
 # bash ~/.local/bin/myscripts/if_void &
 
 pkill sxhkd
