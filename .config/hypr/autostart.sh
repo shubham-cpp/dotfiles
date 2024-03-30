19d278e11d0907859f7ae0b68a37cf0962095e5a
Shubham Pawar <shubhampawar3007@gmail.com>
2024-03-30 09:41:04 +0530

    font changes

--- .config/hypr/autostart.sh
+++ .config/hypr/autostart.sh
@@ -4,9 +4,11 @@ sleep 1
 killall -e xdg-desktop-portal-hyprland
 killall -e xdg-desktop-portal-wlr
 killall xdg-desktop-portal
+xrdb -override ~/.config/X11/Xresources
 /usr/lib/xdg-desktop-portal-hyprland &
 sleep 2
 /usr/lib/xdg-desktop-portal &
 waybar &
 swaync &
 swaybg --image ~/.config/wall.png --mode stretch &
+setsid -f /usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1
